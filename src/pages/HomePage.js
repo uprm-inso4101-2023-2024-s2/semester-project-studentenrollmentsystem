ee3844379bb2c694b6b49716581218bb001c022b
KevinIbarra1 <kevin.ibarra@upr.edu>
2024-02-10 16:03:03 -0400

    Created button component and folder structure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/pages/homePage.module.scss"
+
+export default function HomePage() {
+  return (
+    <div className={styles.Home}>
+      <p>Hello, World! This is an empty homepage.</p>
+    </div>
+  );
+}
