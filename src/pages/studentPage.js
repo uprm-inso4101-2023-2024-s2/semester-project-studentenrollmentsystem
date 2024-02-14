c5e4a05a92390fc5713aecb8514957e8182f4115
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2024-02-14 09:16:23 -0400

    Added collapsible sidebar. Finished page foundation.

--- src/pages/studentPage.js
+++ src/pages/studentPage.js
@@ -5,8 +5,12 @@ function StudentPage() {
   return (
     <>
       <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"></link>
+      <div className="sidebar" id="sidebarid">
+        <i className="bx bx-menu" id="btn2" onClick={closeNav}>
+        </i>
+      </div>
       <div className="topbar" id="topbarid">
-        <i className="bx bx-menu" id="btn1" onMouseEnter={openNav} onMouseLeave={closeNav}>
+        <i className="bx bx-menu" id="btn1" onClick={openNav}>
         </i>
         <span className="btntitle">
           Perfil Estudiantil
@@ -14,10 +18,8 @@ function StudentPage() {
         <span className="user">
           <img src="https://as2.ftcdn.net/v2/jpg/00/64/67/27/1000_F_64672736_U5kpdGs9keUll8CRQ3p3YaEv2M6qkVY5.jpg" className="user-img"></img>
         </span>
-        <nav className="list" id="listid">
-        </nav>
       </div>
-        
+
       <div className="main-page">
         <div className="profile-side">
           <h1>Profile</h1>
@@ -34,11 +36,15 @@ export default StudentPage;
 
 function openNav()
 {
+  document.getElementById("sidebarid").style.width = "10%";
+  document.getElementById("sidebarid").style.opacity = "1";
   document.getElementById("btn1").style.rotate = "90deg";
-  document.getElementById("topbarid").style.height = "10vh";
+  document.getElementById("btn1").style.opacity = "0";
 }
 function closeNav()
 {
+  document.getElementById("sidebarid").style.width = "0";
+  document.getElementById("sidebarid").style.opacity = "0";
   document.getElementById("btn1").style.rotate = "0deg";
-  document.getElementById("topbarid").style.height = "100%";
+  document.getElementById("btn1").style.opacity = "1";
 }
