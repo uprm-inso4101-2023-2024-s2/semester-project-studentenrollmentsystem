0f8978fdc8ee56d9e3c9cb06563354b59c58f4de
KevinIbarra1 <kibarra@haipriority.com>
2024-02-13 22:12:40 -0400

    Implemented reactRouter, fixed navBar and implemented Calendar page

---
+++ src/pages/Calendar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/pages/calendar.module.scss";
+import Navbar from "../components/navbar";
+import Button from "../components/button";
+
+export default function Calendar() {
+  return (
+    <div className={styles.Home}>
+      <div className={styles.header}>
+        <div className={styles.views}>
+          <h2>CALENDAR</h2>
+          <div className={styles.buttons}>
+            <Button>Day</Button>
+            <Button>Month</Button>
+            <Button>Week</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
