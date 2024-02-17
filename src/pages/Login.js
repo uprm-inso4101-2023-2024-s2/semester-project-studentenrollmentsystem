48ddfcbaaefe4c5330089e68f9d186fa98d2f6c8
Frankie113 <francisco.rosario6@upr.edu>
2024-02-16 22:45:40 -0400

    Added Login fields area

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,8 @@ export default function LoginPage() {
   return (
     <div className={styles.loginPage}>
 
-      <div
+      <div class="row">
+        <div class="column"
         className = {styles["vertical-rectangle"]}
         style={{
           backgroundImage: `url(${portico})`,
@@ -19,6 +20,26 @@ export default function LoginPage() {
           left: "5%",
         }}
       ></div>
+
+
+        <div className="column" style={{marginLeft:'370px', marginTop:'160px'}}>
+          <h1 style={{fontSize:'40px'}}>STUDENT LOGIN</h1>
+          <div>
+            <form>
+              <input type="text" id="uname" placeholder="Email" style={{marginBottom:'4px'}}></input><br/>
+              <input type="password" id="password" placeholder="Password"></input><br/>
+              <input type="button" id="login" value='Log in' style={{marginTop:'10px'}}></input>
+            </form>
+          </div>
+          <div style={{display: 'flex', justifyContent:'center', alignContent:'center'}}>
+            <p style={{float:'left', fontSize:'15px', paddingRight:'10px'}}>Don't have an account?</p>
+            <a href="url" style={{textDecoration:'none', fontSize:'17px', textAlign:'center', marginTop:'14px'}}>Sign up</a>
+          </div>
+        </div>
+      </div>
+
+      
+
     </div>
   );
 }
