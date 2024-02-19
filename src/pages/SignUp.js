781e5297189673dd37adffdc52df5cbf65dbfdb9
abdielcotto <abdiel.cotto@upr.edu>
2024-02-19 11:32:59 -0400

    Made the gui look better and made some changes so the user can put their name when creating the profile

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,25 +1,38 @@
 import React from "react";
 import styles from "../styles/pages/signup.module.scss"
-import Button from "../components/button";
-import TextInput from "../components/textinput";
 
 export default function SignUp() {
   return (
-    <div class={styles.SignupBox}>
-      <h2>SIGN UP</h2>
-      <form>
-        <label>Email</label>
-          <input type="email" placeholder="" />
-        <label>New password</label>
-          <input type="password" placeholder="" />
-        <label>Confim password</label>
-          <input type="password" placeholder="" />
-      </form>
-        <input type="button" value="Create Account"></input>
+    <div className={styles.signupContainer}>
+      <div className={styles.SignupBox}>
+        <h2>SIGN UP</h2>
+        <form>
+          <div className={styles.nameFields}>
+            <div className={styles.field}>
+              <label>Name</label>
+              <input type="text" placeholder=""></input>
+            </div>
+            <div className={styles.field}>
+              <label>Last Name</label>
+              <input type="text" placeholder=""></input>
+            </div>
+          </div>
+          <div className={styles.field}>
+            <label>Email</label>
+            <input type="email" placeholder="" />
+          </div>
+          <div className={styles.field}>
+            <label>New password</label>
+            <input type="password" placeholder="" />
+          </div>
+          <div className={styles.field}>
+            <label>Confirm password</label>
+            <input type="password" placeholder="" />
+          </div>
+          <input type="button" value="Create Account" />
+        </form>
         <p>Already have an account? <a href="/login">Login Here</a></p>
+      </div>
     </div>
-    
-    
-    
   );
 }
