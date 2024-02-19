c1d38775a8cd042f68824469d03d0dc26db84138
HectorRivera1 <hector.rivera94@upr.edu>
2024-02-19 17:17:57 -0400

    Added Daily&Monthly versions of the schedule.

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -3,8 +3,11 @@ import styles from "../styles/pages/calendar.module.scss";
 import Button from "../components/button";
 import TextInput from "../components/textinput";
 import Card from "../components/card";
+import DailySchedule from "../components/DailySchedule"; // assuming Schedule component is located in the same directory
+import MonthlySchedule from "../components/MonthlySchedule";
 
 export default function Calendar() {
+
   return (
     <div className={styles.Home}>
       <div className={styles.header}>
@@ -14,9 +17,13 @@ export default function Calendar() {
             <Button>Day</Button>
             <Button>Month</Button>
             <Button>Week</Button>
-          </div>
-        </div>
-      </div>
-    </div>
+          </div> 
+        </div> 
+      </div> 
+
+      <div className={styles.calendarContainer}> {/* Add a container for the calendar */}
+        <DailySchedule /> 
+      </div> 
+    </div>    
   );
 }
