3fa45492e550ba64e436b617dd6e97b335bf8307
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2024-02-21 22:28:48 -0400

    Made dynamic coursetable with csv entries.

--- src/components/courseTable.js
+++ src/components/courseTable.js
@@ -1,63 +1,53 @@
 import React from "react";
 import styles from "../styles/components/courseTable.module.scss";
+import {useState, useEffect} from "react";
+import Data from "../dummydata/dumbdata.csv";
+import Papa from "papaparse"
 
 export default function Coursetable()
 {
+    //Only works with npm papaparse (npm install papaparse)
+    //Will potentially be changed in the future depending on data inputs
+    const [data,setData] = useState([])
+    useEffect(()=> {
+        const fetchData = async()=> {
+            const response = await fetch(Data);
+            const reader = response.body.getReader();
+            const result = await reader.read();
+            const decoder = new TextDecoder("utf-8");
+            const csvData = decoder.decode(result.value);
+            const parsedData = Papa.parse(csvData, {header:true, skipEmptyLines:true}).data;
+            setData(parsedData);
+        };
+        fetchData();
+    }, []);
+
     return(
-        <table className={styles.Coursetable}>
-        <tr>
-            <th>Curso</th>
-            <th>Seccion</th>
-            <th>Creditos</th>
-            <th>Reuniones</th>
-            <th>Profesor</th>
-            <th>Info. Adicional</th>
-        </tr>
-        <tr>
-            <td>MATE4145</td>
-            <td>070</td>
-            <td>4</td>
-            <td>
-                1:00pm-2:50pm LW AE406
-                <br/>1:00pm-1:50pm V AE406
-            </td>
-            <td>
-                Angel Cruz
-            </td>
-            <td>[Redacted]</td>
-        </tr>
-        <tr>
-            <td>A</td>
-            <td>B</td>
-            <td>B</td>
-            <td>C</td>
-            <td>D</td>
-            <td>F</td>
-        </tr>
-        <tr>
-            <td>A</td>
-            <td>B</td>
-            <td>B</td>
-            <td>C</td>
-            <td>D</td>
-            <td>F</td>
-        </tr>
-        <tr>
-            <td>A</td>
-            <td>B</td>
-            <td>B</td>
-            <td>C</td>
-            <td>D</td>
-            <td>F</td>
-        </tr>
-        <tr>
-            <td>A</td>
-            <td>B</td>
-            <td>B</td>
-            <td>C</td>
-            <td>D</td>
-            <td>F</td>
-        </tr>
-        </table>
+        <>
+            {data.length ? (
+                <table className={styles.Coursetable}>
+                    <thead>
+                        <tr>
+                        <th>Curso</th>
+                        <th>Seccion</th>
+                        <th>Creditos</th>
+                        <th>Reuniones</th>
+                        <th>Profesor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((row,index)=>(
+                            <tr key ={index}>
+                                <td>{row.Curso}</td>
+                                <td>{row.Seccion}</td>
+                                <td>{row.Creditos}</td>
+                                <td>{row.Reuniones}</td>
+                                <td>{row.Profesor}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : null}
+        </>
     );
 }
