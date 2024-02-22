536310f8381d81e89bce35abe0212c057f3625ee
diegoperez16 <diego.perez16@upr.edu>
2024-02-22 15:13:13 -0400

    Integrated Card component with CourseList.

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from "../styles/components/card.module.scss";
 
-export default function Card({courseName, credits, instructor, imageUrl}){
+export default function Card({courseName, credits, instructor, imageUrl, description, buttontext = 'Click Me'}){
     return(
         <div className={styles.card}>
         <img src={imageUrl} alt="Card" className={styles.image} />
         <div className={styles.content}>
           <h2 className={styles.courseName}>{courseName}</h2>
           <h1 className={styles.instructor}>{instructor}</h1>
+          <p className={styles.description}>{description}</p>
           <p className={styles.credits}>{credits}</p>
-          <button className={styles.button}>Enroll Now</button>
+          <button className={styles.button}>{buttontext}</button>
         </div>
       </div>
     ); 
--- src/components/courselist.js
+++ src/components/courselist.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Grid from './grid'; // Import the generic Grid component
+import Card from './card.js';
 import styles from '../styles/components/courselist.module.scss'; // Path to your CourseList CSS module
 
 const CourseList = ({ courses }) => {
     return (
         <Grid>
             {courses.map((course) => (
-                <div key={course.id} className={styles.courseItem}>
-                    <h3>{course.title}</h3>
-                    <p>{course.description}</p>
-                </div>
+               <Card
+               key={course.id}
+               imageUrl={course.image}
+               courseName={course.title}
+               instructor={course.instructor}
+               description={course.description} // Extracting instructor assuming format "Instructor, Credits"
+               credits={course.credits}
+               buttontext='Available Sections' // Extracting credits
+                />
             ))}
         </Grid>
     );
