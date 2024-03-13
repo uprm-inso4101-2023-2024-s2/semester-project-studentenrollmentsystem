baf7bdaabe41df86079b9ff6309a9571f4625c98
ChristianMorales25 <christian.morales25@upr.edu>
2024-03-13 02:34:48 -0400

    implemented a page that has all free elective courses

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import FilterTest from './pages/FilterTest';
 import OfferedCourses from './pages/OfferedCourses';
 import SignUp from './pages/SignUp';
 import LoginPage from './pages/Login';
+import FreeElectivesPage from './pages/FreeElectives';
+import FreeElectives from './pages/FreeElectives';
 
 export default function App() {
   return (
@@ -22,6 +24,7 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/filter-test" element={<FilterTest />} />
         <Route path="/offered-courses" element={<OfferedCourses />} />
+        <Route path="/free-electives" element={<FreeElectives />} />
         {/* Add more routes as necessary */}
       </Routes>
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -57,6 +57,9 @@ export default function Navbar() {
         <li onClick={closeNavItem}>
           <Link to="/filter-test">Filter-test</Link>
         </li>
+        <li onClick={closeNavItem}>
+          <Link to="/free-electives">Free Electives</Link>
+        </li>
       </ul>
       <div className={styles.profile}>
         <Link to="/login">
